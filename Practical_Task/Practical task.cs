56b0347d2550ed1a227b6dd62c2d6adc0ecc290c
jay_sk <jay_sk@ngs.ru>
2022-12-02 23:30:16 +0700

    Add answer to task. Entering and printing the first array.

---
+++ Practical_Task/Practical task.cs
@@ -0,0 +1,51 @@
+﻿/*
+Задача: Написать программу, которая из имеющегося массива строк 
+формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+int GetElements(string message) 
+{
+    int digit;
+    while(true)
+    {
+        Console.Write(message);
+        if(int.TryParse(Console.ReadLine(), out int elementsCount) && elementsCount > 0)
+        {
+            digit = elementsCount;
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"{elementsCount} - Введены не корректные данные. Повторите ввод \n");
+        }
+    }
+    return digit;
+} 
+
+void PrintArrayFirstIntroduced(string[] arrayFirstIntroduced)
+{
+    for (int i = 0; i < arrayFirstIntroduced.Length; i++)
+    {
+        Console.Write($"\nВведите элемент массива под индексом {i}: \t");
+        arrayFirstIntroduced[i] = Console.ReadLine();
+    }
+
+Console.WriteLine();
+
+    for (int j = 0; j < arrayFirstIntroduced.Length; j++)
+    {
+        Console.Write($"[{arrayFirstIntroduced[j]}] ");    
+    }
+}
+
+int elementsCount = GetElements("Введите количество элементов массива:\t");
+string[] arrayFirstIntroduced = new string[elementsCount];
+PrintArrayFirstIntroduced(arrayFirstIntroduced);
